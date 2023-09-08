794be8ae6081a86513a45c1fab02d43083bb2731
bsmtt <basmatalaat224@gmail.com>
2023-09-08 16:00:55 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+
+	while (n <= 'z' && n != 'q' && n != 'e')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
