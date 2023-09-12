e359580e545fcd20c9b958055d0bdd3c7887ea5e
bsmtt <basmatalaat224@gmail.com>
2023-09-12 12:32:36 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * print_alphabet - check the code
+ *
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
