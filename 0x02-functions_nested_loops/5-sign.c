20c22f429ca94b3585d7473cbb8a296b3231a701
bsmtt <basmatalaat224@gmail.com>
2023-09-12 15:20:14 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,14 @@ int print_sign(int n)
 	if(n > 0)
 	{
 		putchar(sign);
-		return (1);
 	}
 	else if (n < 0)
 	{
 		printf("-");
-		return (-1);
 	}
 	else
 	{
 		printf("0");
-		return (0);
 	}
+	return 0;
 }
