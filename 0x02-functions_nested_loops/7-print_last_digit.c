4446cdbe428e2c77d130c67ce753338a1eeff885
bsmtt <basmatalaat224@gmail.com>
2023-09-12 16:05:46 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks if a character is lowercase
+ * @n: the integer to check
+ *
+ * Return: n if the integer  is +, -n otherwise
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
