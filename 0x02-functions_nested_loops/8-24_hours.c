b7a15f4f94547837f0656ff235be7553be270da2
bsmtt <basmatalaat224@gmail.com>
2023-09-12 16:58:56 -0700

    last digit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,10 +8,10 @@
 void jack_bauer(void)
 {
 	int h = 0;
-	int m = 0;
 
 	while (h <= 23)
 	{
+		int m = 0;
 		while (m <= 59)
 		{
 			_putchar((h / 10) + '0');
@@ -21,7 +21,7 @@ void jack_bauer(void)
 			_putchar('\n');
 			m++;
 		}
-		m = 0;
+
 		h++;
 	}
 
