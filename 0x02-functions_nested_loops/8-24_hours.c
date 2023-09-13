7bc037421c9555c41bfb05af4af67fb76c0fcd3f
bsmtt <basmatalaat224@gmail.com>
2023-09-12 17:10:34 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,16 @@
 void jack_bauer(void)
 {
 	int h,m;
+	h = 0;
 
-	for (h = 0; h <= 23; h++)
+	while (h <= 23)
 	{
-		for (m = 0; m<= 59; m++)
+		m = 0;
+
+		while (m <= 59)
 		{
 			_putchar((h / 10) + 48);
-			_putchar((m % 10) + 48);
+			_putchar((h % 10) + 48);
 			_putchar(':');
 			_putchar((m / 10) + 48);
 			_putchar((m % 10) + 48);
