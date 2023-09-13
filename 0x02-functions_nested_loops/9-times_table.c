567e6044f1fdf109a2c38ee73874efb776e8d966
bsmtt <basmatalaat224@gmail.com>
2023-09-12 18:58:17 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,13 @@ void times_table(void)
 
 	for (h = 0; h <= 9; h++)
 	{
-		printf("%2d", h * 0);
-		for (m = 1; m <= 9; m++)
+		for (m = 0; m <= 9; m++)
 		{
 			p = h * m;
 			if (m < 9)
-				printf(", %2d", p);
+				printf("%3d, ", p);
 			else
-				printf(", %2d", p);
+				printf("%3d", p);
 
 		}
 		printf("\n");
