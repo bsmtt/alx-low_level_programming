8da5ed2389fa4fabe979d2f77191e6103a57de59
bsmtt <basmatalaat224@gmail.com>
2023-09-12 17:29:34 -0700

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - check the code
+ *
+ * Return: Always void.
+ */
+void times_table(void)
+{
+	int h, m, p;
+
+	h = 0;
+
+	while (h <= 9)
+	{
+		_putchar('0');
+		m = 1;
+
+		while (m <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			p = m * h;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((p / 10) + '0');
+
+			_putchar((p % 10) + 48);
+			m++;
+		}
+		putchar('\n');
+		h++;
+	}
+
+}
