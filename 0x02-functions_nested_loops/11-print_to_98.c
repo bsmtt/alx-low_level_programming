219514646a6424cfdfc2bac82835391b2adfdd3d
bsmtt <basmatalaat224@gmail.com>
2023-09-12 18:03:13 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - check the code
+ *
+ * Return: Always void.
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+		while (n > 98)
+		{
+			_putchar(n);
+			_putchar(', ');
+			n--;
+		}
+	if (n < 98)
+		while (n < 98)
+		{
+			_putchar(n);
+			_putchar(', ');
+			n++;
+		}
+	_putchar('98');
+
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (m = 1; m <= 9; m++)
+		for (m = 0; m <= 9; m++)
 		{
 			_putchar(',');
 			_putchar(' ');
