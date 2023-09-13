5abe2c1655b95258abdfe8403ac7dca9df33dce5
bsmtt <basmatalaat224@gmail.com>
2023-09-12 17:47:58 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: the integer to check
+ * @b: th integer to add
+ * Return: n if the integer  is +, -n otherwise
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
