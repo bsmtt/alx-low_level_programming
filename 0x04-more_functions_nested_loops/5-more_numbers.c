592c13f4428e81626f1cdb8adefaa150f37fcceb
bsmtt <basmatalaat224@gmail.com>
2023-09-14 12:14:01 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always void.
+ */
+void more_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int n = 0;
+		while (n <= 14)
+		{
+			printf("%d", n);
+			n++;	       
+		}
+		putchar('\n');
+		i++;
+	}
+	putchar('\n');
+}
