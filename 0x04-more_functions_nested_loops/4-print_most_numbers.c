7dc02b3eeb729ecc1bc52689fe1fe43cf328ea41
bsmtt <basmatalaat224@gmail.com>
2023-09-14 09:47:33 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always void.
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+			continue;
+
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+}
