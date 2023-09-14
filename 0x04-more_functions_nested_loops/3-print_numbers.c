285370909d90051a3c393f03673286581c814b09
bsmtt <basmatalaat224@gmail.com>
2023-09-14 09:39:04 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always void.
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+}
