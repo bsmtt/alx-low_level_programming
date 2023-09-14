97dac9118577337ee628d4ee554e633bd6f3d8d3
bsmtt <basmatalaat224@gmail.com>
2023-09-14 09:29:25 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - mul two numbers
+ * @a: the integer to check
+ * @b: th integer to mul
+ * Return: n if the integer  is +, -n otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
