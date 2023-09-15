51c578abdce4638a348b02e6a5dd0f5fdabfa053
bsmtt <basmatalaat224@gmail.com>
2023-09-15 16:20:50 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - checks if a character is digit
+ * @n: the character to check
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar('0' + n % 10);
+}
