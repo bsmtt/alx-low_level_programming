0f6f82f35f8a3c50f486d329f23682e5652d512c
bsmtt <basmatalaat224@gmail.com>
2023-09-15 16:30:36 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,17 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
 
-	if (n / 10 > 0)
+	if (num / 10 > 0)
 	{
-		print_number(n / 10);
+		print_number(num/ 10);
 	}
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (num % 10));
 }
