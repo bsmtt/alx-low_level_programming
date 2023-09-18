92961cd1922bb4b196e9bcaf4bd6c9c67df9ffe8
bsmtt <basmatalaat224@gmail.com>
2023-09-18 13:52:32 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ * @str: start point
+ * Return: Always void.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i += 2;
+	}
+
+	printf("\n");
+
+}
