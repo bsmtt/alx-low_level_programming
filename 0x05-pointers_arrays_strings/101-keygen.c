c0be20f4fe2e86c8999c90f5f9eee1d4245e6eee
bsmtt <basmatalaat224@gmail.com>
2023-09-18 15:20:35 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @a: first variable
+ * @b: second variable
+ * Return: Always void.
+ */
+void main(void)
+{
+	char password = rand();
+	printf("Tada! Congrats");
+}
