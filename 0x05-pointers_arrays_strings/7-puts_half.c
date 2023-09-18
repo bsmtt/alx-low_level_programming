7ddf1bc42363e87eaed3a67f9d8efc69b11adcf0
bsmtt <basmatalaat224@gmail.com>
2023-09-18 14:15:34 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: start point
+ * Return: Always void.
+ */
+void puts_half(char *str)
+{
+	int ls;
+	int l = strlen(str);
+
+	if (l % 2 = 0)
+	{
+		ls = l /2;
+	}
+	else
+	{
+		ls = (l - 1) /2;
+	}
+
+	while (ls < l)
+	{
+		printf("%c", str[ls]);
+		i ++;
+	}
+
+	printf("\n");
+
+}
