8a9a12a781ec2ea7b2963590960d85538c03b72a
bsmtt <basmatalaat224@gmail.com>
2023-09-18 13:11:33 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: start point
+ * Return: Always void.
+ */
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int i = length - 1;
+	int j = 0;
+	char t;
+
+	while (i >= 0 && j < length)
+	{
+		t = *s[j];
+		*s[j] = *s[i];
+		*s[i] = t;
+		j++;
+		i--;
+	}
+
+}
