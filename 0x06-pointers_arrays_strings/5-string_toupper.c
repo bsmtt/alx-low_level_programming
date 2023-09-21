3d377c7297bff8726b221bb7c2628458b874e373
bsmtt <basmatalaat224@gmail.com>
2023-09-21 12:18:36 -0700

    cat

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * string_toupper - check the code
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ * @str: second variable
+ * Return: Always void.
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (isspace(*ptr) || ispunct(*ptr))
+		{
+			char next_char = *(ptr + 1);
+			*(ptr + 1) = toupper(next_char);
+		}
+		ptr++;
+	}
+	return (str);
+}
