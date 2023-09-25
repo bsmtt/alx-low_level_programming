5fa70085616c6f13f38677aa6b8b675ac3279d6c
bsmtt <basmatalaat224@gmail.com>
2023-09-25 16:22:46 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strpbrk - cpy memory byte
+ * @haystack:first variable
+ * @needle: second variable
+ * Return: s or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return haystack;
+	}
+
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return haystack;
+		}
+
+		haystack++;
+	}
+	return (NULL);
+}
