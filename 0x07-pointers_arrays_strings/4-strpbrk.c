3978045dd9664ca50584d14b2d7d4cb55cc659a0
bsmtt <basmatalaat224@gmail.com>
2023-09-25 15:51:51 -0700

    strprk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - cpy memory byte
+ * @s:first variable
+ * @accept: second variable
+ * Return: s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*accept == *s)
+				return (s);
+
+			accept++;
+		}
+		s++;
+	}
+	return (NULL);
+}
