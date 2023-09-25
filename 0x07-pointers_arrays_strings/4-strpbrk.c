2379545eed3f8a8949ee1e1c445f56894d782002
bsmtt <basmatalaat224@gmail.com>
2023-09-25 15:54:03 -0700

    strprk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,9 +8,9 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s != '\0')
+	while (*s >= '\0')
 	{
-		while (*accept != '\0')
+		while (*accept >= '\0')
 		{
 			if (*accept == *s)
 				return (s);
