44cd4b7b7673e8bfd04bde1123b9796eed5a30e3
bsmtt <basmatalaat224@gmail.com>
2023-09-25 16:39:29 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - cpy memory byte
+ * @a:first variable
+ * Return void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+
+	while (i < 8)
+	{
+		int j = 0;
+
+		while (j < 8)
+		{
+			printf("%c ", a[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
