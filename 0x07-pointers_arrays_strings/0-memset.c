88e148a89d9291bdd6f0f6184783dafd58d9acfc
bsmtt <basmatalaat224@gmail.com>
2023-09-25 12:22:41 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - check the code
+ * @s: second variable
+ * @b: btyte
+ * @n: count
+ * Return: Always void.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while(i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
