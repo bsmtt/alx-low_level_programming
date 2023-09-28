c3f08f81af868f0c2843a5bddd78b622f422f9a9
bsmtt <basmatalaat224@gmail.com>
2023-09-28 15:16:41 -0700

    factiorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - checks if a character is digit
+ * @n: the character to check
+ *
+ * Return: int
+ */
+int factorial(char n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return n * factorial(n - 1);
+}
