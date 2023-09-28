8a16e32fdb53a0d8980ac1e3d07b66e096753f10
bsmtt <basmatalaat224@gmail.com>
2023-09-28 16:33:00 -0700

    fractional

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - checks if a character is digit
+ * @x: the  value
+ * @y: the power
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
