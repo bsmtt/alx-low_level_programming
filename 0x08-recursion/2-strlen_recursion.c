a0c4d45005396c96cbc755ac96c5de61f4d3e6e2
bsmtt <basmatalaat224@gmail.com>
2023-09-28 14:45:37 -0700

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - checks if a character is digit
+ * @s: the character to check
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	return 1 + _strlen_recursion(s + 1);
+}
