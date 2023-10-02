1d6753c6df2a423598d994e69a7eb3efb409e9f4
bsmtt <basmatalaat224@gmail.com>
2023-10-02 13:52:42 -0700

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argv: second variable
+ * @argc: btyte
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i <= argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
