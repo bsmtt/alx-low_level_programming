b7796d27bc4d927419ff779ff479de2b5058bda7
bsmtt <basmatalaat224@gmail.com>
2023-10-02 14:14:41 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int result = atoi(argv[1]) * atoi(argv[2]);
+	result = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", result);
 	return (0);
