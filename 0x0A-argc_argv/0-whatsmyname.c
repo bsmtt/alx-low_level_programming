d96ebfaf72ce4b80d39287f47c5be3976e685dc4
bsmtt <basmatalaat224@gmail.com>
2023-10-02 13:33:39 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,12 @@
  * main - check the code
  * @argv: second variable
  * @argc: btyte
- * Return: int
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
