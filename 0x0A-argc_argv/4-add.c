903895995db5b6445e43f4c65c55f460fe6f655f
bsmtt <basmatalaat224@gmail.com>
2023-10-02 14:33:34 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argv: second variable
+ * @argc: btyte
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	int i = 0;
+	int sum;
+
+	while (i < argc)
+	{
+		int j;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+				printf("Error\n");
+				return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
