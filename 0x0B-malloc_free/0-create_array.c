d4ea9da3e630ecda7fc43bb209420e47d1b4cbaa
bsmtt <basmatalaat224@gmail.com>
2023-10-04 12:59:33 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,14 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
+
 	array = (char *) malloc(size * sizeof(char));
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
