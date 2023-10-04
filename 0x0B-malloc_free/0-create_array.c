7d1a36ff3a46b15134dda1752c1cb9cb29391ef8
bsmtt <basmatalaat224@gmail.com>
2023-10-04 12:47:41 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - checks if a character is lowercase
+ * @c: the integer to check
+ * @size : array size
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	char *array = (char *) malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return NULL;
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return array;
+}
