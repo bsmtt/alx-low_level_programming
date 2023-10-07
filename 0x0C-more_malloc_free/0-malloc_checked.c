b9bda3181a426091d0f0090f8839ee4e5474410c
bsmtt <basmatalaat224@gmail.com>
2023-10-07 14:29:21 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - checks if a character is lowercase
+ * @b: the string
+ * Return: char
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+
+
+	return (ptr);
+}
