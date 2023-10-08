1707be05d06f14cfa89af5109c43891fdb1693bd
bsmtt <basmatalaat224@gmail.com>
2023-10-08 13:09:48 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 
 /**
  * _calloc - checks if a character is lowercase
- * @sie: the string
+ * @size: the string
  * @nmemb : concat
  * Return: char
  */
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 
