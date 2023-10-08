d786c6adf45183c73d894dd3a357325872786b46
bsmtt <basmatalaat224@gmail.com>
2023-10-08 13:33:36 -0700

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * array_range - checks if a character is lowercase
+ * @max: the string
+ * @min : concat
+ * Return: char
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size = max - min + 1;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+
+	arr = malloc(size * sizeof(int));
+	if (arr != NULL)
+	{
+		int i;
+
+		for(i = 0; i < size; i++)
+		{
+			arr[i] = min + i;
+		}
+	}
+
+
+	return (arr);
+}
