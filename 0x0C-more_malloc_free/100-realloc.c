1da16f6b73b40220734b939a1fff20bb6b86f31c
bsmtt <basmatalaat224@gmail.com>
2023-10-09 13:28:54 -0700

    rreal loc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
 	unsigned int min_size;
+
 	if (new_size == 0)
 	{
 		free(ptr);
@@ -23,7 +24,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (malloc(new_size));
 	}
-	
+
 	if (new_size == old_size)
 	{
 		return (ptr);
