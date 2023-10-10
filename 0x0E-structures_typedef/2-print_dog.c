cb09d20b44e80987fb32c2211b1922a187b1ed34
bsmtt <basmatalaat224@gmail.com>
2023-10-10 16:42:49 -0700

    init dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - check the code
+ * @d : d
+ * Return: Always void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+
+	}
+}
