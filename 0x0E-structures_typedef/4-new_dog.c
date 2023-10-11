b508046be0506e07e038695a782e4f72f1c62a70
bsmtt <basmatalaat224@gmail.com>
2023-10-10 17:12:29 -0700

    new god

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - check the code
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: Always void.
+ */
+dog_t new_dog(char *name, float age, char *owner)
+{
+    if (name == NULL || owner == NULL) {
+        return NULL;
+    }
+
+    dog_t *new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL) {
+        return NULL;
+    }
+
+    new_dog->name = malloc(strlen(name) + 1);
+    if (new_dog->name == NULL) {
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->name, name);
+
+    new_dog->age = age;
+
+    new_dog->owner = malloc(strlen(owner) + 1);
+    if (new_dog->owner == NULL) {
+        free(new_dog->name);
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(new_dog->owner, owner);
+
+    return new_dog;
+}
