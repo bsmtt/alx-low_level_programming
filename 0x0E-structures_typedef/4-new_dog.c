b2d8b59644cc4cca39ca6a54b54af6d8e1531346
bsmtt <basmatalaat224@gmail.com>
2023-10-10 17:23:31 -0700

    new god

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,31 +14,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-    if (name == NULL || owner == NULL) {
-        return NULL;
-    }
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL) {
-        return NULL;
-    }
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
 
-    new_dog->name = malloc(strlen(name) + 1);
-    if (new_dog->name == NULL) {
-        free(new_dog);
-        return NULL;
-    }
-    strcpy(new_dog->name, name);
+	new_dog->name = malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
 
-    new_dog->age = age;
+	new_dog->age = age;
 
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
-    strcpy(new_dog->owner, owner);
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
 
-    return new_dog;
+	return (new_dog);
 }
