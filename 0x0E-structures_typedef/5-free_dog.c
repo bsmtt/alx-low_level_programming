de8f436808bcf42896752ecf394c27842c9057b5
bsmtt <basmatalaat224@gmail.com>
2023-10-10 17:31:31 -0700

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,8 +7,10 @@
  * @d : d
  * Return: Always void.
  */
-void free_dog(dog_t *d) {
-	if (d != NULL) {
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
 		free(d->name);
 		free(d->owner);
 		free(d);
