01b3e2c905e53ed24941def46a1a6c221f894686
bsmtt <basmatalaat224@gmail.com>
2023-10-11 15:40:24 -0700

    int indx

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+/**
+ * int_index - check if the absolute value of a numbe
+ * @array: the integer to check
+ * @size: size
+ * @cmp: function
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
+
