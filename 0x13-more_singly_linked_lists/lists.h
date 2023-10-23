eb219f6db9443059454d5171e032576ef33409f4
bsmtt <basmatalaat224@gmail.com>
2023-10-23 12:55:15 -0700

    print lisrint

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_t - A structure representing a node in a linked list.
+ * @n: The data stored in the node.
+ * @next: A pointer to the next node in the list.
+ */
+typedef struct listint_t
+{
+	int n;
+	struct listint_t *next;
+} listint_t;
+
+#endif
