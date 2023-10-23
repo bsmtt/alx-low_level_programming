cfc61950c0fb67bdbe4b9fc788e02c2ca3aab456
bsmtt <basmatalaat224@gmail.com>
2023-10-23 13:35:14 -0700

    fix: listint len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,12 +2,12 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t linked list.
+ * listint_len - Prints all the elements of a listint_t linked list.
  * @h: A pointer to the head of the linked list.
  *
  * Return: The number of nodes in the linked list.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	if (h == NULL)
 	{
