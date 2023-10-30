e700da4a6c82535a79b4b95767cae13ddc65b94e
bsmtt <basmatalaat224@gmail.com>
2023-10-30 15:40:34 -0700

    read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text from a file and prints it
+ * @filename: name of file
+ * @letters: letters count
+ *
+ * Return: number
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	ssize_t size;
+	char *buffer;
+
+	if (!filename || !letters)
+	{
+		return (0);
+	}
+
+	buffer = (char *) malloc(letters + 1);
+	if (!buffer)
+	{
+		return (0);
+	}
+
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
+
+	size = read(fp, buffer, letters);
+	size = write(STDOUT_FILENO, buffer, sizze);
+
+	return (size);
+}
