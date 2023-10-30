39ae636d90d41b2452d0eeaa439e3a94c243180d
bsmtt <basmatalaat224@gmail.com>
2023-10-30 16:40:36 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
 /**
  * create_file - reads text from a file and prints it
