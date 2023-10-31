72464c71293a9622bf53c7572331bdf73f531b1a
bsmtt <basmatalaat224@gmail.com>
2023-10-30 17:03:27 -0700

    create file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  *
  * Return: number
  */
-int append_text_to_file(const char *filename, char *text_content) 
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes_written;
